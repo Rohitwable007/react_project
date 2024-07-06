13cc5f50478eb7dafe26468f820802ed87f96dfd
MacBook Pro <a1@MacBook-Pro.local>
2024-07-06 09:25:31 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -4,19 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <header >
+       
+        
+           <h2>Welcome To HMIS </h2>
+           <p>
+            Hospital Infromation Management system
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
